9e25ced7b4e9a527b159771a75f3b77e720dcdfb
leeyanggoo <didrn94553@gmail.com>
2023-07-24 21:22:49 +0900

    env 재설정

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,65 @@
+import { signOut } from "next-auth/react";
+import { BiLogOut } from "react-icons/bi";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
+import SidebarTweetButton from "./SidebarTweetButton";
+
+const Sidebar = () => {
+  const { data: currentUser } = useCurrentUser();
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+      auth: true,
+      alert: currentUser?.hasNotification,
+    },
+    {
+      label: "Profile",
+      href: `/users/${currentUser?.id}`,
+      icon: FaUser,
+      auth: true,
+    },
+  ];
+
+  return (
+    <div className="h-full col-span-1 pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              auth={item.auth}
+              alert={item.alert}
+            />
+          ))}
+          {currentUser && (
+            <SidebarItem
+              onClick={() => signOut()}
+              icon={BiLogOut}
+              label="Logout"
+            />
+          )}
+
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
